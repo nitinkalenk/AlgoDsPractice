1772ae6fe76cbc5486f645467d5465b3b4d41233
nkale <nitinkale.nk@gmail.com>
2022-11-07 15:50:15 +0530

    two sum

---
+++ src/com/practice/array/TwoSum.java
@@ -0,0 +1,19 @@
+package com.practice.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> noToIndexMap = new HashMap();
+    for(int i = 0; i < nums.length; i++) {
+      if(noToIndexMap.containsKey(target - nums[i])) {
+        return new int[] {noToIndexMap.get(target - nums[i]), i};
+      }
+      noToIndexMap.put(nums[i], i);
+    }
+    return new int[]{};
+  }
+
+}

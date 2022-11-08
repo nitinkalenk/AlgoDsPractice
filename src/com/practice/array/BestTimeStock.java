368e6288294287099725c5435d22f40cb6e2c056
nkale <nitinkale.nk@gmail.com>
2022-11-08 17:02:07 +0530

    best time to buy stock

---
+++ src/com/practice/array/BestTimeStock.java
@@ -0,0 +1,15 @@
+package com.practice.array;
+
+public class BestTimeStock {
+
+  public int maxProfit(int[] prices) {
+    int min = prices[0];
+    int maxProfit = 0;
+    for(int i = 1; i < prices.length; i++) {
+      maxProfit = Math.max(maxProfit, prices[i] - min);
+      min = Math.min(min, prices[i]);
+    }
+    return maxProfit;
+  }
+
+}

59253f20ea738631d5a638e98dacb40ab7a60376
nkale <nitinkale.nk@gmail.com>
2022-11-10 11:48:02 +0530

    move zeros

---
+++ src/com/practice/array/MoveZeros.java
@@ -0,0 +1,17 @@
+package com.practice.array;
+
+public class MoveZeros {
+
+  public void moveZeroes(int[] nums) {
+    int currentNonZeroIndex = 0;
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[i] != 0) {
+        nums[currentNonZeroIndex++] = nums[i];
+      }
+    }
+    for(int i = currentNonZeroIndex; i < nums.length; i++) {
+      nums[i] = 0;
+    }
+  }
+
+}

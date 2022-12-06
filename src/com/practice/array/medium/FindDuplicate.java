34f6860d379258efe0f960bd9315f703448e7f28
nkale <nitinkale.nk@gmail.com>
2022-12-06 18:46:43 +0530

    find duplicate

---
+++ src/com/practice/array/medium/FindDuplicate.java
@@ -0,0 +1,21 @@
+package com.practice.array.medium;
+
+public class FindDuplicate {
+
+  // https://leetcode.com/problems/find-the-duplicate-number/submissions/
+  public int findDuplicate(int[] nums) {
+    int slow = nums[0];
+    int fast = nums[nums[0]];
+    while(slow != fast) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    }
+    slow = 0;
+    while(slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+    return slow;
+  }
+
+}

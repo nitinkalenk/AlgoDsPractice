d6f81419c86e7b57e1f8e46d6024595e7a1b4499
nkale <nitinkale.nk@gmail.com>
2022-12-21 23:10:53 +0530

    is palindrome

---
+++ src/com/practice/recursion/easy/IsPalindrome.java
@@ -0,0 +1,25 @@
+package com.practice.recursion.easy;
+
+public class IsPalindrome {
+
+  //https://leetcode.com/problems/valid-palindrome/
+  public boolean isPalindrome(String s) {
+    int start = 0;
+    int end = s.length() - 1;
+    while(start <= end) {
+      while(start <= end && !Character.isLetterOrDigit(s.charAt(start))) {
+        start++;
+      }
+      while(start <= end && !Character.isLetterOrDigit(s.charAt(end))) {
+        end--;
+      }
+      if(start <= end && Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+        return false;
+      }
+      start++;
+      end--;
+    }
+    return true;
+  }
+
+}

9091b08584e4f48dfa096e628e840cb90e734a80
nkale <nitinkale.nk@gmail.com>
2023-02-27 12:59:46 +0530

    MaxLengthRepeatedSubarray

---
+++ src/com/practice/dp/medium/MaxLengthRepeatedSubarray.java
@@ -0,0 +1,20 @@
+package com.practice.dp.medium;
+
+public class MaxLengthRepeatedSubarray {
+
+  //https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+  public int findLength(int[] nums1, int[] nums2) {
+    int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+    int ans = 0;
+    for(int i = 1; i < dp.length; i++) {
+      for(int j = 1; j < dp[i].length; j++) {
+        if(nums1[i-1] == nums2[j-1]) {
+          dp[i][j] = dp[i-1][j-1] + 1;
+          ans = Math.max(dp[i][j], ans);
+        }
+      }
+    }
+    return ans;
+  }
+
+}
